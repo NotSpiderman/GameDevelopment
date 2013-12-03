bce01836ff97c8498400d71cdbb1ed97fe92c9d9
Max Mitschke <maximo.anthony.mitschke@gmail.com>
2013-12-02 21:32:47 -0500

    Uploading new scripts for game handlers

---
+++ Game1/Assets/Scripts/JumpHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpHandler : MonoBehaviour {
+	private Vector3 movement;
+	private float distanceToGround;
+	public float jumpSpeed;
+	public float jumpHeight;
+	
+	bool IsGrounded(){
+		return Physics.Raycast(this.transform.position, -Vector3.up, distanceToGround + 0.1f);
+	}
+	
+	// Use this for initialization
+	void Start () {
+		distanceToGround = this.collider.bounds.extents.y;
+		movement = new Vector3();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()){
+			movement.y = jumpHeight;
+		}
+		else if(Input.GetKeyUp(KeyCode.Space)){
+			movement.y = 0;
+		}
+		
+		if(IsGrounded()){
+			this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + movement, Time.deltaTime * jumpSpeed);
+		}
+		else{
+			this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + movement - Physics.gravity, Time.deltaTime * jumpSpeed);
+		}
+	}
+}
---
+++ Game1/Assets/Scripts/KeyHandler.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyHandler : MonoBehaviour {
+	public float force;
+	public float speed;
+	
+	public Vector3 direction;
+	public bool isMoving;
+	
+	void Update(){
+		
+		if(Input.GetKeyDown(KeyCode.W)){
+			direction = Vector3.forward;
+			isMoving  = true;
+			force = speed * 100;
+		}
+		else if(Input.GetKeyUp(KeyCode.W)){
+			direction = -Vector3.forward;
+			force = 0;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.S)){
+			direction = -Vector3.forward;
+			force = speed * 100;
+		}
+		else if(Input.GetKeyUp(KeyCode.S)){
+			direction = Vector3.forward;
+			force = 0;
+		}
+	}
+	
+	void FixedUpdate(){
+		this.rigidbody.AddForce(direction * force * Time.deltaTime);
+	}
+//	public float speed;
+//	public float jumpHeight;
+//	
+//	private Vector3 movement;
+//	
+//	void Start(){
+//		movement = new Vector3();
+//	}
+//	
+//	void Update(){
+//		float moveHorizontal = Input.GetAxis("Horizontal");
+//		float moveVertical   = Input.GetAxis("Vertical");
+//		
+//		movement.x = moveHorizontal;
+//		movement.z = moveVertical;
+//		
+//		this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + movement, Time.deltaTime * speed);
+//	}
+}
